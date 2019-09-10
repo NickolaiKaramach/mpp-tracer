94158f167ebd6433d077a5ebadf23aba3116389b
Николай <nick.karamach@gmail.com>
2019-09-10 23:25:52 +0300

    Add foo action classes

---
+++ mpp-tracer/ActionClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace mpp_tracer
+{
+    public class ActionClass
+    {
+        public static void Job1()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name}:Job1 started");
+            
+            Thread.Sleep(1000);
+            
+            Console.WriteLine($"{Thread.CurrentThread.Name}:Job1 ended");
+        }
+
+        public static void Job2()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name}:Job2 started");
+            
+            Job1();
+            Thread.Sleep(1000);
+            
+            Console.WriteLine($"{Thread.CurrentThread.Name}:Job2 ended");
+        }
+
+        public static void Job3()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name}:Job3 started");
+            
+            Thread.Sleep(1000);
+            Job2();
+            
+            Thread.Sleep(1000);
+            
+            Console.WriteLine($"{Thread.CurrentThread.Name}:Job3 ended");
+        }
+    }
+}
--- mpp-tracer/Program.cs
+++ mpp-tracer/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using System.Threading;
 
 namespace mpp_tracer
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Thread thread1 = new Thread(() => ActionClass.Job3());
+            Thread thread2 = new Thread(() => ActionClass.Job3());
+            
+            thread1.Name = "Thread#1 ";
+            thread2.Name = "Thread#2 ";
+            
+            thread1.Start();
+            Thread.Sleep(500);
+            thread2.Start();
+            
+            thread1.Join();
+            thread2.Join();
         }
     }
 }
